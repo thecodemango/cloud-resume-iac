0f401c6f6633151e636b0360c97444db35ace8b8
thecodemango <thecodemango@gmail.com>
2025-02-03 09:57:47 -0500

    editing comment

--- src/test_put_item.py
+++ src/test_put_item.py
@@ -16,7 +16,7 @@ event2 = {
 }
 class TestPutItem(unittest.TestCase):
     def test_values(self):
-        #A value of 0 is placed as second argument in place for the context paramter used by the lambda functions
+        #A value of 0 is placed as second argument in place for the context paramter used by the lambda function
         #Test for negative value input
         self.assertRaises(ValueError, lambda_handler, event1,0)
         #Test for decimal value input
